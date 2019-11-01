5cd7e77f69c6ca44862f5bd637d46f13c155bd4d
Guillermo Cruz <GuillermoCruz32>
2019-11-01 18:37:29 -0400

    Made some UI changes to the app. Added a dark background to make the app feel as though it is in "Nightmode", and also added the username of each tweet, and made the Twitter handle more noticeable. Also included the gif walk through.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -61,17 +61,20 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
         ImageView ivProfileImage;
         TextView tvBody;
         TextView tvScreenName;
+        TextView tvHandle;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
             tvBody = itemView.findViewById(R.id.tvBody);
             tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvHandle = itemView.findViewById(R.id.tvHandle);
         }
 
         public void bind(Tweet tweet) {
             tvBody.setText(tweet.body);
             tvScreenName.setText(tweet.user.screenName);
+            tvHandle.setText(tweet.user.name);
             Glide.with(context).load(tweet.user.imageURL).into(ivProfileImage);
         }
     }
